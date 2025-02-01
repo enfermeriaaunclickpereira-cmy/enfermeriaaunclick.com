a22fe8358dcda9db5f7f275fe0e49fb52e953936
santiaj018 <santyleivaprt1@gmail.com>
2025-02-01 16:08:38 -0500

    s

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-<script src="script.js"></script>
-</body>
-</html>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sobre Ti</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div id="content">
-        <h2>Sobre Ti</h2>
-        <p>Aquí puedes agregar el contenido que deseas mostrar sobre ti...</p>
-    </div>
-</body>
-</html>
-
+----------------
 
 <!DOCTYPE html>
 <html lang="es">
