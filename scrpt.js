6214ad4540798a4d05375262fb1793fd0f7393a9
santiaj018 <santyleivaprt1@gmail.com>
2025-02-01 02:24:43 -0500

    Añadido reproductor de música

---
+++ scrpt.js
@@ -0,0 +1,6 @@
+function openTab(tabId) {
+    document.querySelectorAll('.tab').forEach(tab => {
+        tab.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+}
