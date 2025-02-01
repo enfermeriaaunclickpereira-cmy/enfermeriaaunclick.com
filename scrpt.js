3481f3c27a8eee57eab49007ad825ab8b83172c8
santiaj018 <santyleivaprt1@gmail.com>
2025-02-01 13:20:50 -0500

    amennnnn

--- scrpt.js
+++ scrpt.js
@@ -1,12 +1,10 @@
 function loadContent(file) {
-    // Usamos fetch para cargar el archivo HTML cuando se hace clic en un botón
     fetch(file)
         .then(response => response.text())  // Convertir la respuesta en texto
         .then(data => {
-            // Cargar el contenido en el área específica
-            document.getElementById("content-area").innerHTML = data;
+            document.getElementById("content-area").innerHTML = data; // Mostrar contenido en el área
         })
         .catch(error => {
-            console.error('Error al cargar el contenido:', error);
+            console.error('Error al cargar el contenido:', error); // Mostrar errores en la consola
         });
 }
